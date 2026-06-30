dc929d18ece3b02ba06c32d99e77de2f1433316e
Ahamdrezaariyanasab <ahmadrezaariyanasab@gmail.com>
2026-06-30 19:54:15 +0330

    Mini Profile Generator

--- Day01/student_score.py
+++ Day01/student_score.py
@@ -1,1 +1,25 @@
-print("Hello Ai Engineer 🚀 ")
+#print("Hello Ai Engineer 🚀 ")
+#name=input("what's your name ?").strip().title()
+#remove whitespace frome str and capitalize user's name
+#name=name.strip().title()
+#capitalize user's name
+#name=name.capitalize()
+#capitalize title user's name
+#name=name.title()
+#say hello to user
+#print(f"hello, {name}")
+
+#print("Name:", "Ahmadreza", "City:", "Esfahan", sep=" | ", end=" <END>\n")
+
+def main():
+    name=input("what's your name ?")
+    city=input("which city do you live in ?")
+    age=input("how old are you ?")
+    occupation=input("what do you do ?")
+    hello(name,city,age,occupation)
+
+def hello(name,city,age,occupatin):
+    output=f"Hello , {name} , {age} years old {occupatin} from {city} city .".title()
+    print(output)
+
+main()
